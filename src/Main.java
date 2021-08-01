77ed0f951a988499502d2899e75103c37aace1e4
Evgeniya Zavyalova <evgeniyazavyalova95@gmail.com>
2021-08-01 17:20:19 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 31290;
+        int price= 20;
+        int mili = amount/price;
+        System.out.println(mili);
+    }
+}
+
